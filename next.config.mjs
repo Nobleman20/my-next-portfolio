3dd55e60b8809ec0c3bda43cd66006b4005175a2
Nobleman20 <emmanueloluyege@gmail.com>
2024-11-13 02:58:37 +0100

    my first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com", // Example hostname
+        pathname: "/**", // Adjust based on actual image path
+      },
+    ],
+  },
+};
 
 export default nextConfig;
